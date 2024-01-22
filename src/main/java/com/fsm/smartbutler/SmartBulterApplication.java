77939e2d636aeabf2561b887bb9b1e929e31df7a
firstdapin <firstdapin1203@gmail.com>
2024-01-22 19:01:29 +0900

    project_start

---
+++ src/main/java/com/fsm/smartbutler/SmartBulterApplication.java
@@ -0,0 +1,13 @@
+package com.fsm.smartbutler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBulterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBulterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsm/smartbulter/SmartBulterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsm.smartbulter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBulterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
