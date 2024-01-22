6d9aa108893579d4a74c1e93f90e84cd26252a53
firstdapin <firstdapin1203@gmail.com>
2024-01-22 19:12:18 +0900

    프로젝트 준비

--- src/main/java/com/fms/smartbutler/SmartBulterApplication.java
+++ src/main/java/com/fms/smartbutler/SmartBulterApplication.java
@@ -1,4 +1,4 @@
-package com.fsm.smartbutler;
+package com.fms.smartbutler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/fms/smartbutler/controller/user/TestController.java
+++ src/main/java/com/fms/smartbutler/controller/user/TestController.java
@@ -1,4 +1,4 @@
-package com.fsm.smartbutler.controller.user;
+package com.fms.smartbutler.controller.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
