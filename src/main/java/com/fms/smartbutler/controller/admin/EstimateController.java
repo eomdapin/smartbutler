da83f8ba9c7524f25be40842594c444c0fe93c61
jeongssi <greatt726@gmail.com>
2024-01-23 18:30:07 +0900

    css수정 다수 : 수정 회원가입, 마이페이지, 견적신청상세 수정입니다.

--- src/main/java/com/fms/smartbutler/controller/admin/EstimateController.java
+++ src/main/java/com/fms/smartbutler/controller/admin/EstimateController.java
@@ -8,12 +8,12 @@ public class EstimateController {
 
 	@GetMapping("/admin/estimate/list")
 	public String estimateList() {
-		return "admin/user/estimate-list";
+		return "admin/estimate/estimate-list";
 	}
 	
 	@GetMapping("/admin/estimate/info")
 	public String estimateInfo() {
-		return "admin/user/estimate-info";
+		return "admin/estimate/estimate-info";
 	}
 	
 }
