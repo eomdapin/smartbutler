c7786b3b295d1d3375c9de1e0edb1f920e9af135
firstdapin <firstdapin1203@gmail.com>
2024-01-23 12:25:24 +0900

    의존성 추가 lombok, mysql, jpa 적용

---
+++ src/main/java/com/fms/smartbutler/service/BuildService.java
@@ -0,0 +1,22 @@
+package com.fms.smartbutler.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fms.smartbutler.dto.Build;
+import com.fms.smartbutler.repository.BuildRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BuildService {
+	
+	private final BuildRepository buildRepository;
+	
+	public void insert(Build build) {
+		log.info("buildRepository : {}", buildRepository);
+		buildRepository.save(build);
+	}
+}
