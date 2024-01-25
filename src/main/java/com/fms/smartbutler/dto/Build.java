750afc38eee23089a0fb71343a412bcb506a10b3
eomdapin <qlsekdja2@gmail.com>
2024-01-25 11:15:27 +0900

    build-info.html 수정 사용자 건물 정보 보기에서 DB내용 가져오기 구현, 이미지 정보 불러오기 성공

--- src/main/java/com/fms/smartbutler/dto/Build.java
+++ src/main/java/com/fms/smartbutler/dto/Build.java
@@ -1,5 +1,6 @@
 package com.fms.smartbutler.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,11 +18,25 @@ public class Build {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long buildId;
+	
+	@Column(name = "build_name")
 	private String buildName;
+
+	@Column(name = "address")
 	private String address;
+	
+	@Column(name = "floor")
 	private Integer floor;
+	
+	@Column(name = "room")
 	private Integer room;
+	
+	@Column(name = "imgId")
 	private Long imgId;
+	
+	@Column(name = "area")
 	private Integer area;
+	
+	@Column(name = "com_date")
 	private String comDate;
 }
