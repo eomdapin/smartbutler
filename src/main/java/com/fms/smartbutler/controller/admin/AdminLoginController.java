939b60132f8105ce39e6c2a52116fc9cc9c7d0cd
firstMyId <jeonjongdae5@gmail.com>
2024-01-26 16:57:49 +0900

    company & resident views:수정 경로 수정

--- src/main/java/com/fms/smartbutler/controller/admin/AdminLoginController.java
+++ src/main/java/com/fms/smartbutler/controller/admin/AdminLoginController.java
@@ -1,8 +1,11 @@
 package com.fms.smartbutler.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
@@ -12,6 +15,14 @@ public class AdminLoginController {
 	@GetMapping("/login")
 	public String getlogin() {
 		return "admin/login/login";
+	}
+	
+	// 관리자 로그인
+	@PostMapping("/login")
+	public String login(@RequestParam("id") String id, @RequestParam("password") String password, Model model) {
+		model.addAttribute("id", id);
+		model.addAttribute("password", password);
+		return "/";
 	}
 	
 }
