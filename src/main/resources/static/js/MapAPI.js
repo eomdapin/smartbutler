fe53349714695ff00847c36ad0ed926fde22f18d
eomdapin <qlsekdja2@gmail.com>
2024-01-26 23:06:52 +0900

    MapAPI.js 추가 카카오지도 API 추가 완료 건물 정보에 따라 동적으로 변경되게 적용

---
+++ src/main/resources/static/js/MapAPI.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    let container = document.getElementById('buildMap'); //지도를 담을 영역의 DOM 레퍼런스
+    let address = document.getElementById('address').value;
+    let buildName = document.getElementById('buildName').value;
+    let inputDisabled = document.getElementsByTagName('input');
+
+    let options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    map.setDraggable(false);
+    map.setZoomable(false);
+
+    // 주소-좌표 변환 객체를 생성합니다
+    let geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address, function (result, status) {
+        // 정상적으로 검색이 완료됐으면 
+        if(status === kakao.maps.services.Status.OK) {
+            let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            let infowindow = new kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">'+ buildName +'</div>'
+            });
+            infowindow.open(map, marker);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        }
+    });
+
+    for(let i = 0; i < inputDisabled.length; i++) {
+        inputDisabled[i].disabled = true;
+    }
+}
