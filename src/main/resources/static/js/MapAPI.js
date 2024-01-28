4ef32b2cfcfbeeffb7d97a9437146195086e9027
eomdapin <qlsekdja2@gmail.com>
2024-01-28 17:26:46 +0900

    MapAPI.js 수정 주석 삭제

--- src/main/resources/static/js/MapAPI.js
+++ src/main/resources/static/js/MapAPI.js
@@ -11,28 +11,22 @@ window.onload = function () {
 
     let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    // 주소-좌표 변환 객체를 생성합니다
     let geocoder = new kakao.maps.services.Geocoder();
 
-    // 주소로 좌표를 검색합니다
     geocoder.addressSearch(address, function (result, status) {
-        // 정상적으로 검색이 완료됐으면 
         if(status === kakao.maps.services.Status.OK) {
             let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-            // 결과값으로 받은 위치를 마커로 표시합니다
             let marker = new kakao.maps.Marker({
                 map: map,
                 position: coords
             });
 
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
             let infowindow = new kakao.maps.InfoWindow({
                 content: '<div style="width:150px;text-align:center;padding:6px 0;">'+ buildName +'</div>'
             });
             infowindow.open(map, marker);
 
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
             map.setCenter(coords);
         }
     });
