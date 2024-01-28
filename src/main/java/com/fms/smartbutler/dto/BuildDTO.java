3354e075952c519d99e7763297e98ae143cde9f2
eomdapin <qlsekdja2@gmail.com>
2024-01-28 18:02:09 +0900

    BuildDTO.java 수정 필드 삽입

--- src/main/java/com/fms/smartbutler/dto/BuildDTO.java
+++ src/main/java/com/fms/smartbutler/dto/BuildDTO.java
@@ -1,5 +1,19 @@
 package com.fms.smartbutler.dto;
 
-public class BuildDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+public class BuildDTO {
+	
+	private Long buildId;
+	private String buildName;
+	private String address;
+	private Integer floor;
+	private Integer room;
+	private Integer area;
+	private String comDate;
 }
