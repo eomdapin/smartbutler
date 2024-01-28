4cb5ebdb8f2645620c528b7fcd3cb953609ceece
trueshot3 <kingsv12@gmail.com>
2024-01-29 00:01:45 +0900

    ModelMapper 추가, ClaimDTO로 변경, 쿼리문 파라미터 보이게 설정
    claim 뷰 수정, 이미지 연동

--- src/main/java/com/fms/smartbutler/controller/user/UserClaimController.java
+++ src/main/java/com/fms/smartbutler/controller/user/UserClaimController.java
@@ -1,7 +1,6 @@
 package com.fms.smartbutler.controller.user;
 
-import java.sql.Date;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -12,8 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fms.smartbutler.domain.Claim;
+import com.fms.smartbutler.domain.Image;
+import com.fms.smartbutler.dto.ClaimDTO;
 import com.fms.smartbutler.service.ClaimService;
+import com.fms.smartbutler.service.ImageService;
+import com.fms.smartbutler.vo.FileVo;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,11 +25,12 @@ import lombok.RequiredArgsConstructor;
 public class UserClaimController {
 
 		private final ClaimService claimService;
+		private final ImageService imageService;
 
 		// 민원 목록
 		@GetMapping
 		public String getClaimList(Model model) {
-			List<Claim> claims = claimService.findAll();
+			List<ClaimDTO> claims = claimService.findAll();
 			
 			model.addAttribute("claims", claims);
 			
@@ -37,9 +40,16 @@ public class UserClaimController {
 		// 민원 상세
 		@GetMapping("/{claimId}")
 		public String getClaimInfo(@PathVariable Long claimId, Model model) {
-			Claim claim = claimService.findById(claimId).get();
+			ClaimDTO claimDTO = claimService.findById(claimId).orElseGet(ClaimDTO::new);
+			List<Image> images = imageService.findByOutIdAndCoded(claimDTO.getClaimId(), "c");
+			FileVo vo = new FileVo();
 			
-			model.addAttribute("claim", claim);
+			if(images.size() > 0) {
+				vo.setFileName(images.get(0).getRealName());
+			}
+			
+			model.addAttribute("claim", claimDTO);
+			model.addAttribute("vo", vo);
 			
 			return "user/claim/claim-info";
 		}
@@ -47,18 +57,29 @@ public class UserClaimController {
 		// 민원 등록 폼
 		@GetMapping("/add")
 		public String getClaimAdd(Model model) {
-			LocalDate localDate = LocalDate.now();
-			Date today = Date.valueOf(localDate);
+			List<String> options = new ArrayList<String>();
 			
-			model.addAttribute("today", today);
+			options.add("전기");
+			options.add("수도");
+			options.add("냉난방기");
+			options.add("엘리베이터");
+			options.add("기타");
+			
+			model.addAttribute("options", options);
 			
 			return "user/claim/claim-add";
 		}
 		
 		// 민원 등록
 		@PostMapping("/add")
-		public String postClaimAdd(@ModelAttribute Claim claim) {
-			claimService.insert(claim);
+		public String postClaimAdd(@ModelAttribute ClaimDTO claimDTO, @ModelAttribute FileVo vo) throws Exception {
+			claimService.insert(claimDTO);
+			
+			if(!vo.getFileName().isEmpty()) {
+				Image image = new Image();
+				image.getImageCategory().setCoded("c");
+				imageService.saveImage(vo, image, claimDTO.getClaimId());
+			}
 			
 			return "redirect:/user/claims";
 		}
--- src/main/java/com/fms/smartbutler/service/ClaimService.java
+++ src/main/java/com/fms/smartbutler/service/ClaimService.java
@@ -2,10 +2,13 @@ package com.fms.smartbutler.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.fms.smartbutler.domain.Claim;
+import com.fms.smartbutler.dto.ClaimDTO;
 import com.fms.smartbutler.repository.ClaimRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,20 +18,37 @@ import lombok.RequiredArgsConstructor;
 public class ClaimService {
 
 	private final ClaimRepository claimRepository;
+	private final ModelMapper modelMapper;
 	
-	public void insert(Claim claim) {
+	public void insert(ClaimDTO claimDTO) {
+		Claim claim = modelMapper.map(claimDTO, Claim.class);
+		
+		claimRepository.save(claim);
+		claimDTO.setClaimId(claim.getClaimId());
+	}
+	
+	public void update(ClaimDTO claimDTO) {
+		Claim claim = modelMapper.map(claimDTO, Claim.class);
+		
 		claimRepository.save(claim);
 	}
 	
-	public void update(Claim claim) {
-		claimRepository.save(claim);
+	public Optional<ClaimDTO> findById(Long claimId) {
+		Optional<Claim> claim = claimRepository.findById(claimId);
+		ClaimDTO claimDTO = modelMapper.map(claim, ClaimDTO.class);
+		
+		return Optional.ofNullable(claimDTO);
 	}
 	
-	public Optional<Claim> findById(Long claimId) {
-		return claimRepository.findById(claimId);
-	}
-	
-	public List<Claim> findAll() {
-		return claimRepository.findAll();
+	public List<ClaimDTO> findAll() {
+		List<Claim> claimList = claimRepository.findAll();
+		List<ClaimDTO> claimDTOList = claimList
+										.stream()
+										.map(c -> 
+											modelMapper
+											.map(c, ClaimDTO.class))
+											.collect(Collectors.toList());
+		
+		return claimDTOList;
 	}
 }
