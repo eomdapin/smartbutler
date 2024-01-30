1698c8b49ede82c07f46fdb613e49860bcd668c4
eomdapin <qlsekdja2@gmail.com>
2024-01-30 21:22:49 +0900

    cost-add.html 추가 관리비 목록 및 추가 기능 완료

--- src/main/java/com/fms/smartbutler/config/AdminSecurityConfig.java
+++ src/main/java/com/fms/smartbutler/config/AdminSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/fms/smartbutler/controller/admin/CostController.java
+++ src/main/java/com/fms/smartbutler/controller/admin/CostController.java
@@ -1,18 +1,23 @@
 package com.fms.smartbutler.controller.admin;
 
-import java.util.List;
-
 /**
 * @author 엄다빈
 * @editDate 2024-01-30 ~ 2024-02-03
 */
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fms.smartbutler.dto.BuildDTO;
 import com.fms.smartbutler.dto.CostDTO;
+import com.fms.smartbutler.service.BuildService;
 import com.fms.smartbutler.service.CostService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,19 +30,34 @@ import lombok.extern.slf4j.Slf4j;
 public class CostController {
 	
 	private final CostService costService;
-
+	private final BuildService buildService;
+	
+	// 관리비 목록
 	@GetMapping("/{buildId}/costs")
-	public String getCostList(Model model) {
+	public String getCostList(@PathVariable Long buildId, Model model) {
 		List<CostDTO> costs = costService.findAll();
-		
 		model.addAttribute("costs", costs);
-		
-//		log.info("costs.get(0).getCostId() :: {}", costs.get(0).getCostId());
-//		log.info("costs.get(0).getBuildId() :: {}", costs.get(0).getBuildId());
-//		log.info("costs.get(0).getElectricity() :: {}", costs.get(0).getElectricity());
-//		log.info("costs.get(0).getCostKinds().get(0).getElectricity() :: {}", costs.get(0).getCostKinds().get(0));
+		model.addAttribute("buildId", buildId);
 		
 		return "admin/cost/cost-list";
 	}
 	
+	// 관리비 입력
+	@GetMapping("/{buildId}/costs/add")
+	public String gerAddCost(@PathVariable Long buildId, @ModelAttribute CostDTO costDTO, Model model) {
+		BuildDTO buildDTO = buildService.findById(buildId);
+		
+		model.addAttribute("build", buildDTO);
+		
+		return "admin/cost/cost-add";
+	}
+	
+	// 관리비 저장
+	@PostMapping("/{buildId}/costs/add")
+	public String postAddCost(@PathVariable Long buildId, @ModelAttribute CostDTO costDTO) {
+		costDTO.setBuildId(buildId);
+		costService.save(costDTO);
+		
+		return "redirect:/admin/buildings/{buildId}/costs";
+	}
 }
