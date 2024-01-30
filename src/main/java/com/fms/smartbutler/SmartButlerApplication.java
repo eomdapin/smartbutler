e47cd322998f56619bb9f1ff5c7e6b8a05c102a8
jbkr <jb_kr@yahoo.co.kr>
2024-01-31 01:36:01 +0900

    securityEnable

--- src/main/java/com/fms/smartbutler/SmartButlerApplication.java
+++ src/main/java/com/fms/smartbutler/SmartButlerApplication.java
@@ -2,9 +2,8 @@ package com.fms.smartbutler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SmartButlerApplication {
 
 	public static void main(String[] args) {
