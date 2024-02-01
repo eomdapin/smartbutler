215afd6d313c4deaf95b75ffb181f1e4f32dff93
jeongssi <greatt726@gmail.com>
2024-02-01 13:01:01 +0900

    견적 기능 : 수정 견적 목록, 상세, 수정

--- src/main/java/com/fms/smartbutler/service/EstimateService.java
+++ src/main/java/com/fms/smartbutler/service/EstimateService.java
@@ -1,5 +1,9 @@
 package com.fms.smartbutler.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +27,31 @@ public class EstimateService {
 		
 		estimateRepository.save(estimate);
 		estimateDTO.setEstimateId(estimate.getEstimateId());
+	}
+	
+	public void update(EstimateDTO estimateDTO) {
+		Estimate estimate = modelMapper.map(estimateDTO, Estimate.class);
+		
+		estimateRepository.save(estimate);
+		estimateDTO.setEstimateId(estimate.getEstimateId());
+	}
+	
+	public Optional<EstimateDTO> findById(Long estimateId) {
+		Optional<Estimate> estimate = estimateRepository.findById(estimateId);
+		EstimateDTO estimateDTO = modelMapper.map(estimate, EstimateDTO.class);
+		
+		return Optional.ofNullable(estimateDTO);
+	}
+	
+	public List<EstimateDTO> findAll() {
+		List<Estimate> estimateList = estimateRepository.findAll();
+		List<EstimateDTO> estimateDTOList = estimateList
+												.stream()
+												.map(e ->
+													modelMapper
+													.map(e, EstimateDTO.class))
+													.collect(Collectors.toList());
+		return estimateDTOList;
 	}
 	
 }
