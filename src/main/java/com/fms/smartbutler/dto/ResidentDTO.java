a4421e2a4824a488c56c782fe71e5ae44309fc11
eomdapin <qlsekdja2@gmail.com>
2024-02-01 18:04:30 +0900

    Build.java 수정 연결 컬럼명 수정

--- src/main/java/com/fms/smartbutler/dto/ResidentDTO.java
+++ src/main/java/com/fms/smartbutler/dto/ResidentDTO.java
@@ -1,5 +1,22 @@
 package com.fms.smartbutler.dto;
 
-public class ResidentDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+public class ResidentDTO {
+	private Long residentId;
+	private Long buildId;
+	private String buildName;
+	private Long userId;
+	private String phone;
+	private String userName;
+	private int status;
+	private String fromDate;
+	private String toDate;
+	private Long deposit;
+	private Long monthly;
 }
