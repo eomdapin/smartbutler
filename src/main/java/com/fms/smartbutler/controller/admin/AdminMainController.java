5a8cce25654e858055a1d000621f1b02eeb4a003
eomdapin <qlsekdja2@gmail.com>
2024-02-03 19:48:08 +0900

    관리자 main.html, AdminMainController.java 수정 제작 중 중간 백업 미처리 견적, 민원 불러오는 기능 추가

--- src/main/java/com/fms/smartbutler/controller/admin/AdminMainController.java
+++ src/main/java/com/fms/smartbutler/controller/admin/AdminMainController.java
@@ -1,7 +1,18 @@
 package com.fms.smartbutler.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fms.smartbutler.dto.BuildDTO;
+import com.fms.smartbutler.dto.ClaimDTO;
+import com.fms.smartbutler.dto.EstimateDTO;
+import com.fms.smartbutler.service.BuildService;
+import com.fms.smartbutler.service.ClaimService;
+import com.fms.smartbutler.service.CostService;
+import com.fms.smartbutler.service.EstimateService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,10 +20,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminMainController {
 	
+	private final BuildService buildService;
+	private final EstimateService estimateService;
+	private final ClaimService claimService; 
+	private final CostService costService;
+	
 	@GetMapping("/admin")
-	public String getAdminMainPage() {
+	public String getAdminMainPage(Model model) {
+		List<BuildDTO> builds = buildService.findAll();
+		List<EstimateDTO> estimates = estimateService.findAllByConfirm(1);
+		List<ClaimDTO> claims = claimService.findAllByStatus(1);
 		
+		int estimateCnt = estimates.size();
+		int claimCnt = claims.size();
 		
-		 return "admin/main/main";
+		model.addAttribute("estimates", estimates);
+		model.addAttribute("estimateCnt", estimateCnt);
+		model.addAttribute("claims", claims);
+		model.addAttribute("claimCnt", claimCnt);
+		
+		return "admin/main/main";
 	}
 }
