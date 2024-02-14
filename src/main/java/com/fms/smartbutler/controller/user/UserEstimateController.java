dfe94193bdcb69b759283e37a97b6728d38b3ef3
jeongssi <greatt726@gmail.com>
2024-02-14 12:50:46 +0900

    마이페이지 및 견적신청 일부 수정 마이페이지 정보 모두 나오게 변경 견적신청페이지 일부 수정

--- src/main/java/com/fms/smartbutler/controller/user/UserEstimateController.java
+++ src/main/java/com/fms/smartbutler/controller/user/UserEstimateController.java
@@ -1,5 +1,7 @@
 package com.fms.smartbutler.controller.user;
 
+import java.security.Principal;
+
 /**
 * @author 정시운
 * @editDate 2024-01-31 ~ 2024-02-02
@@ -17,8 +19,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fms.smartbutler.dto.BuildDTO;
 import com.fms.smartbutler.dto.EstimateDTO;
+import com.fms.smartbutler.dto.UsersDTO;
 import com.fms.smartbutler.service.BuildService;
 import com.fms.smartbutler.service.EstimateService;
+import com.fms.smartbutler.service.UsersService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,16 +33,19 @@ public class UserEstimateController {
 	
 	private final EstimateService estimateService;
 	private final BuildService buildService;
+	private final UsersService usersService;
 	
 	// 견적 신청 폼
 	@GetMapping
-	public String getEstimate(@RequestParam(required = false) Long buildId, Model model) {
+	public String getEstimate(@RequestParam(required = false) Long buildId, Principal principal, Model model) {
 		buildId = buildId == null ? 1 :buildId;
 		BuildDTO buildDTO = buildService.findById(buildId);
 		List<BuildDTO> build = buildService.findAll();
+		UsersDTO user = usersService.findByEmail(principal.getName()).orElseGet(UsersDTO::new);
 		
 		model.addAttribute("build", buildDTO);
 		model.addAttribute("builds", build);
+		model.addAttribute("user", user);
 		
 		return "user/estimate/estimate-add";
 	}
